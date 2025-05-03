53324b6cb949df4bd379a1247228d167894bccb3
Franklin <1833214@alu.murciaeduca.es>
2025-05-03 23:02:10 +0200

    login defenitivo (faltan templates)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,32 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('usuario@demo.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->hasher->hashPassword($user, '123456'));
+        $manager->persist($user);
 
-        $manager->flush();
+        $host = new User();
+        $host->setEmail('host@demo.com');
+        $host->setRoles(['ROLE_HOST']);
+        $host->setPassword($this->hasher->hashPassword($host, '123456'));
+        $manager->persist($host);
+
+        $manager->flush(); // ✅ Guardar cambios en la base de datos
     }
 }
