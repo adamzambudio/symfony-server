53a7c7183a70fdc138dc3ffddc69e99a90a5cb2b
Franklin <1833214@alu.murciaeduca.es>
2025-05-04 19:00:39 +0200

    Login Y Inicio Hecho con templates

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -38,7 +38,7 @@ class SecurityController extends AbstractController
     #[Route('/user/dashboard', name: 'user_dashboard')]
     public function userDashboard(): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('inicio.html.twig');
         // return $this->render('user/dashboard.html.twig');
     }
 
